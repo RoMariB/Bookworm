0c8613659d8c9fa2d602c9fd1248eeec90278e17
Maria Romanchuk <mashulya_r_88@mail.ru>
2023-02-24 21:15:01 +0700

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Мария Романчук on 24.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
